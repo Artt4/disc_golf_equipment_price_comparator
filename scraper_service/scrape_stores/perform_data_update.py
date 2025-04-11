0dbbaee1e21f7d4e1891559ef71e6229e0db1253
Aortta <44739519+Artt4@users.noreply.github.com>
2025-04-11 15:54:31 +0300

    small fix to perform data_update

--- scraper_service/scrape_stores/perform_data_update.py
+++ scraper_service/scrape_stores/perform_data_update.py
@@ -3,7 +3,7 @@ from scrape_stores.bulk_page_stucture.par3_scrape import get_data_par3
 from scrape_stores.bulk_page_stucture.innovaeurope_scrape import get_data_discsport
 from scrape_stores.bulk_page_stucture.diskiundiski_scrape import get_data_diskiundiskicesis
 from scrape_stores.bulk_page_stucture.powergrip_scrape import get_data_powergrip_from_bulk
-#from app.scrape_stores.single_page_structure.latitude64_scrape import run_latitude64_scraper
+#from scrape_stores.single_page_structure.latitude64_scrape import run_latitude64_scraper
 
 
 def run_all_scrapers():
